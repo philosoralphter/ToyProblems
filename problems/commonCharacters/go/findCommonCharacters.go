83a1a7077837240e2184a1c41d5a8a2fa629eab3
Ralph Samuel <theralphsamuel@gmail.com>
2018-04-24 07:19:11 -0700

    Clarifuy go common chars.

--- problems/commonCharacters/go/findCommonCharacters.go
+++ problems/commonCharacters/go/findCommonCharacters.go
@@ -6,13 +6,13 @@ import "regexp"
 func FindCommonChars (str1 string, str2 string) string {
    resultString := ""
 
-   wordChar, err := regexp.Compile("[a-zA-Z]")
+   wordCharRegex, err := regexp.Compile("[a-zA-Z]")
    if err != nil {
      println("could not parse regexp")
    }
 
   for _, char := range str1 {
-    if strings.ContainsRune(str2, char) && !strings.ContainsRune(resultString, char) && wordChar.MatchString(string(char)){
+    if strings.ContainsRune(str2, char) && !strings.ContainsRune(resultString, char) && wordCharRegex.MatchString(string(char)){
       resultString += string(char)
     }
   }
