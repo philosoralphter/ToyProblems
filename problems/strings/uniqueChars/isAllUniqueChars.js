407136caccaa0a0b4e69c30772105422bad933d6
Ralph Samuel <theralphsamuel@gmail.com>
2018-05-14 03:18:46 -0700

    Complete unique characters in js.

--- problems/strings/uniqueChars/isAllUniqueChars.js
+++ problems/strings/uniqueChars/isAllUniqueChars.js
@@ -22,7 +22,24 @@ function isAllUniqueCharsSmall(...strings) {
         for (let j = 0; j < strings[i].length; j++) {
             let char = strings[i][j];
 
-
+            for (let k = i; k < strings.length; k++) {
+                for (let l = j+1; i < strings[k].length; l++) {
+                    let otherChar = strings[k][l];
+                    if (char === otherChar) return false;
+                }
+            }
         }
     }
+
+    return true;
 }
+
+(function test() {
+    let string1 = 'abcdefghijklmnopqrstuvwxyz';
+    let string2 = 'abc';
+    let string3 = 'def';
+    let string4 = 'def';
+
+    console.log(isAllUniqueCharsFast(string4, string3, string2))
+
+})();
