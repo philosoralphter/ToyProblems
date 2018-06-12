65f2b3e0e90211045e4ce64c1ee4e85ffd410066
Ralph Samuel <theralphsamuel@gmail.com>
2018-06-11 19:51:00 -0700

    Fix isAllUniqueChars in js.

--- problems/strings/uniqueChars/isAllUniqueChars.js
+++ problems/strings/uniqueChars/isAllUniqueChars.js
@@ -1,5 +1,5 @@
 
-
+//O(n) space, O(n)time
 function isAllUniqueCharsFast (...strings) {
     let seenChars = {};
 
@@ -17,15 +17,16 @@ function isAllUniqueCharsFast (...strings) {
     return true;
 }
 
+//O(1) Space, O(n!) time
 function isAllUniqueCharsSmall(...strings) {
     for (let i = 0; i < strings.length; i++) {
         for (let j = 0; j < strings[i].length; j++) {
             let char = strings[i][j];
 
-            for (let k = i; k < strings.length; k++) {
-                for (let l = j+1; i < strings[k].length; l++) {
-                    let otherChar = strings[k][l];
-                    if (char === otherChar) return false;
+
+            for (let  k=i; k<strings.length; k++){
+                for (let l= (k===i ? j+1 : 0); l<strings[k].length; l++) {
+                    if (char === strings[k][l]) return false;
                 }
             }
         }
@@ -35,11 +36,14 @@ function isAllUniqueCharsSmall(...strings) {
 }
 
 (function test() {
-    let string1 = 'abcdefghijklmnopqrstuvwxyz';
-    let string2 = 'abc';
-    let string3 = 'def';
-    let string4 = 'def';
 
-    console.log(isAllUniqueCharsFast(string4, string3, string2))
+    let strings1 = ['abc', 'def','ghi','jkl'];
+    let strings2 = ['abc', 'cde','efg','ghi'];
 
-})();
+    console.log('Testing strings1 fast (should be true): ', isAllUniqueCharsFast(...strings1));
+    console.log('Testing strings1 small (should be true): ', isAllUniqueCharsSmall(...strings1));
+
+    console.log('Testing strings2 fast (should be false): ', isAllUniqueCharsFast(...strings2));
+    console.log('Testing strings2 small (should be false): ', isAllUniqueCharsFast(...strings2));
+
+})()
